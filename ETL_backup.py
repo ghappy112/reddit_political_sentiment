eeec06521e8c76f658dd18852d1b554ec7feb860
ghappy112 <65770357+ghappy112@users.noreply.github.com>
2024-09-30 10:33:59 -0500

    Update ETL_backup.py

--- ETL_backup.py
+++ ETL_backup.py
@@ -131,7 +131,7 @@ df["candidate"] = candidate
 df = df[["author", "created_utc", "text", "score", "candidate", "polarity", "subjectivity", "pk"]]
 
 
-conn = sqlite3.connect("reddit_political_sentiment_database.db")
+conn = sqlite3.connect("database/reddit_political_sentiment_database.db")
 cursor = conn.cursor()
 
 
